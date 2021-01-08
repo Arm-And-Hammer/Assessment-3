270195a0d52dc3366f67116e3b1112ef6dfee5b4
Arm-And-Hammer <armeenafsharjavan@hotmail.com>
2021-01-08 15:01:58 -0500

    3rd commit idk where im at

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Widgit(models.Model):
     item = models.CharField(max_length=50)
 
-    
+    def __str__(self):
+        return self.item
+
+    def get_absolute_urls(self):
+        return reverse('index')
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from .models import Widgit
 
 # Create your views here.
+
+def index(request):
+    widgits = Widgit.objects.all()
+    return render(request, 'index.html', {'widgits': widgits})
+
+# class WidgitCreate(CreateView):
+#     model = widgits
+#     fields = "__all__"
+    
+
+def delete_widgit(request, widgit_id):
+    Widgit.objects.get(id=widgit_id).delete()
+    return redirect('/')
